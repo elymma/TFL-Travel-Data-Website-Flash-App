78b110b25b198f506ffbd157734290eef5abe7c7
elymma <elymma.mensah.19@ucl.ac.uk>
2022-03-20 00:11:11 +0000

    factory pattern applied to Flask app

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Flask app example with a basic structure
 
 A minimal app to demonstrate how to structure and configure a Flask app.
+
+App should be run using the app.run() method
---
+++ first_app/app.py
@@ -0,0 +1,12 @@
+from first_app import create_app
+
+app = create_app()
+
+
+@app.route('/')
+def index():
+    return 'This is the home page for my_flask_app'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
