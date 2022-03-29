200fcdce5cfb378b4085f358eb04b07ed6ff0655
elymma <elymma.mensah.19@ucl.ac.uk>
2022-03-29 19:27:48 +0100

    auth form with working returns and route

--- first_app/auth/forms.py
+++ first_app/auth/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField
-from wtforms.validators import DataRequired, EqualTo, Length, Email
+from wtforms.validators import DataRequired, EqualTo, Length
 
 
 class SignupForm(FlaskForm):
     first_name = StringField(label="First name", validators=[DataRequired("First name must be entered!"), Length(max=20, message="Maximum characters reached!")])
     last_name = StringField(label="Last name", validators=[DataRequired("Last name must be entered!"), Length(max=20, message="Maximum characters reached!")])
     email = EmailField(label="Email address", validators=[DataRequired("Email address must be entered!")])
-    password = PasswordField(label="Password", validators=[DataRequired("A password must be entered!")])
+    password = PasswordField(label="Password", validators=[DataRequired("A password must be entered!"), Length(min=8, max=20, message="Password must be between 8 and 20 characters long!")])
     password_repeat = PasswordField(label="Repeat Password",
                                     validators=[DataRequired("Repeat password!"), EqualTo("password", message="Entered passwords must match!")])
+
+
--- first_app/auth/routes.py
+++ first_app/auth/routes.py
@@ -1,11 +1,19 @@
 from flask import Blueprint, render_template
+from first_app.auth.forms import SignupForm
 
-auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+auth_bp = Blueprint("auth", __name__, url_prefix="/signup")
 
 
-@auth_bp.route("/")
-def index():
-    return render_template("signup.html", title="Profile")
+@auth_bp.route("/", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.first_name.data
+        return f"Congrats {name}, you are signed up to the TFL Travel Dashboard!"
+
+    return render_template("signup.html", title="Sign Up", form=form)
+
+
 
 
 
