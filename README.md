dd96975513ce43bd04fb4f5c21b3294500032156
elymma <elymma.mensah.19@ucl.ac.uk>
2022-04-20 15:07:36 +0100

    markdown file edited

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Flask app example with a basic structure
 
 [Elymma's repository](https://github.com/elymma/Coursework2.git)
+
+Before running the app, first run [setup.py](./setup.py) then install the required packages.
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='my_app',
-    version='1.0.0',
+    name='first_app',
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
+        "flask",
     ],
 )
 
